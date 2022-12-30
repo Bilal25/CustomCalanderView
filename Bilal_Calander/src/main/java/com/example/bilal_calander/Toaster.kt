e35dd84864402f0fe3471de4f1371468864eae70
bilalali181 <syebilal.ezhire@gmail.com>
2022-12-30 18:03:34 +0500

    first

---
+++ Bilal_Calander/src/main/java/com/example/bilal_calander/Toaster.kt
@@ -0,0 +1,13 @@
+package com.example.bilal_calander
+
+import android.content.Context
+import android.widget.Toast
+
+class Toaster {
+
+    fun Toastview(ctx : Context){
+        Toast.makeText(ctx,"Code copied", Toast.LENGTH_SHORT).show()
+    }
+
+
+}
---
+++ app/src/main/java/com/example/bilal_custom_calander/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bilal_custom_calander
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
